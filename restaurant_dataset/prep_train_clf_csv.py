cd2ac7cb7189659077ccf10b9574998602ddab30
Chris Czarnecki <cczarnec@uwaterloo.ca>
2023-09-27 13:33:13 -0400

    Dataset prep script for the restaurant dataset

---
+++ restaurant_dataset/prep_train_clf_csv.py
@@ -0,0 +1,44 @@
+# %% Prepare the training data for classification
+# This is specifically meant for the restaurant dataset
+# %%
+import pandas as pd
+from pathlib import Path
+
+# %%
+
+df = pd.read_csv("train.csv", header=0)
+
+# %%
+
+df.hist(column="revenue", bins=100)
+
+# %%
+
+df["revenue"].describe()
+
+# %%
+
+df["revenue"].quantile(0.6)
+
+# %%
+# Take the 60% of the data with the highest revenue and classify it as "high revenue"
+df.loc[df["revenue"] > df["revenue"].quantile(0.6), "high_revenue"] = True
+
+# %%
+# See if it worked
+df.head()
+# %%
+# Take all missing values in the "high_revenue" column and classify them as "low revenue"
+df.loc[df["high_revenue"].isna(), "high_revenue"] = False
+
+# %%
+
+# See if it worked
+df.head()
+
+# %%
+# Export the dataframe to a CSV file
+
+df.to_csv("train_clf.csv", index=False)
+
+# %%
